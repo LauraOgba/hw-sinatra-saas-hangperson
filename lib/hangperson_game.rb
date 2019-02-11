629635db8ce9ec002c5cee76bc945d6e2e7739c1
EC2 Default User <ec2-user@ip-172-31-2-105.us-east-2.compute.internal>
2019-02-11 12:27:57 +0000

    Part 2

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -15,6 +15,50 @@ class HangpersonGame
     @guesses = ''
     @wrong_guesses = ''
   end
+  
+   def guess(char)
+    if char == nil or not char =~ /^[a-zA-Z]$/i 
+      raise ArgumentError 
+    end
+    
+    # change chars to downcase
+    char.downcase!
+
+
+ if @word.include? char and not @guesses.include? char
+      @guesses.concat char
+      return true
+    elsif not @word.include? char and not @wrong_guesses.include? char
+      @wrong_guesses.concat char
+      return true
+    else
+      return false
+    end
+  end
+  
+   def word_with_guesses
+    result = ''
+    
+     @word.each_char do |char|  
+      result.concat '-' unless @guesses.include? char
+      result.concat char if @guesses.include? char
+    end
+    return result
+  end
+  
+  def check_win_or_lose
+    guess = self.word_with_guesses
+
+ if guess == '-' or @wrong_guesses.length == 7
+      :lose
+    elsif guess == @word and @wrong_guesses.length < 7
+      :win
+    else
+      :play
+    end
+  end
+  
+ 
 
   # You can test it by running $ bundle exec irb -I. -r app.rb
   # And then in the irb: irb(main):001:0> HangpersonGame.get_random_word
@@ -27,5 +71,5 @@ class HangpersonGame
       return http.post(uri, "").body
     }
   end
-
+  
 end
